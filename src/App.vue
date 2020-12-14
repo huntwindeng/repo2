53c835955794ce1044aa0e6671438163318bde4c
huntwindeng <huntwin@163.com>
2020-12-14 10:59:32 +0800

    首次使用ideagit

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+<!--    <img src="./assets/logo.png">-->
+    <router-view/>
+  </div>
+</template>
+
+<script>
+    import VueAxios from 'vue-axios'
+    import axios from 'axios'
+export default {
+  name: 'App',
+    data(){
+        return{
+
+        }
+    },
+    // fetch实现跨域
+    //实现跨域
+    created() {
+        // fetch方式实现跨域
+        this.axios.post("/api/showList").then(res=>{
+            console.log(res)
+        })
+    }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/test/Test.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+      <p>恭喜您测试成功，你可以开心的写代码了，码农哥哥！</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Test"
+    }
+</script>
+
+<style scoped>
+
+</style>
